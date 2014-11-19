eeff5fb202edd7d268298b45b965e77317b9142c
unknown <DRIA@ST-L-0038.tessella.co.uk>
2014-11-19 00:43:48 +0000

    Update LED blinking file with PWM.

--- blinkLED.py
+++ blinkLED.py
@@ -1,11 +1,17 @@
 import RPi.GPIO as GPIO  
 import time  
 # blinking function  
-def blink(pin):  
+def blink(pin, ratio=0.5):
+        ratio = 0 if ratio < 0 else ratio
+        ratio = 1 if ratio > 1 else ratio
+
+        time_on = 1 * ratio
+        time_off = 1 * (1 - ratio)
+
         GPIO.output(pin,GPIO.HIGH)  
-        time.sleep(1)  
+        time.sleep(time_on)  
         GPIO.output(pin,GPIO.LOW)  
-        time.sleep(1)  
+        time.sleep(time_off)  
         return  
 
 # to use Raspberry Pi board pin numbers  
@@ -15,6 +21,9 @@ pin_number = 11
 
 GPIO.setup(pin_number, GPIO.OUT)  
 # blink GPIO17 50 times  
-for i in range(0,50):  
-        blink(pin_number)  
-GPIO.cleanup() 
+try:
+    for i in range(0, 20):  
+        blink(pin_number, 0.75)
+
+finally:
+    GPIO.cleanup()
