1ae69e7d22c3a6af2e5dc67c02201916c1372d25
Anniek van der Drift <anniek.vanderdrift@tessella.com>
2014-11-23 01:44:06 +0000

    Update bottle script that returns VirtualLED website and acts on xmlhttprequests.

--- returnWebsite.py
+++ returnWebsite.py
@@ -1,6 +1,12 @@
-from bottle import route, run, static_file
+from bottle import route, run, static_file, debug, request, response
 import RPi.GPIO as GPIO
+import json
 
+# Create global variable for LED status and initialise to false
+LedStatus = False
+
+#enable bottle debug
+debug(True)
 
 GPIO.setmode(GPIO.BOARD) 
 
@@ -23,6 +29,29 @@ def uit():
 @route('/On')
 def aan():
 	GPIO.output(led, True)
+
+@route('/SetStatus', method='POST')
+def setStatus():
+    print "POST Header : \n %s" % dict(request.json) #for debug header
+
+    data = request.json;
+    status  = data['LedStatus']
+
+    global LedStatus
+    LedStatus = status
+    print "LedStatus : \n %s" % str(LedStatus)
+
+    GPIO.output(led, LedStatus)
+
+@route('/GetStatus')
+def getStatus():
+    
+    # Use this if the global variable set in setStatus should be used
+    #return { "LedStatus" : str(LedStatus)}
+    
+    # determines the state from the raspberry pi pin
+    state = GPIO.input(led)
+    return { "LedStatus" : str(state)}
 
 try:
  run(host='0.0.0.0', port=8080, debug=True, reloader=True)
