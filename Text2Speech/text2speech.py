52ba5fbafac5a3cf507f81698400d0cd94ba9ee8
Anniek <anniekvanderdrift@hotmail.com>
2014-11-30 21:51:16 +0000

    Add text to speech file using espeak. Split call to googletranlate into sentences to stay below character limit.

--- Text2Speech/text2speech.py
+++ Text2Speech/text2speech.py
@@ -33,33 +33,63 @@ def speakSpeechFromText(phrase):
     googleSpeechURL = getGoogleSpeechURL(phrase)
     subprocess.call(["mplayer",googleSpeechURL], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+def currentWeatherUrl():
+    queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Stevenage,uk&APPID='
+    return queryUrl
 
+def forecastDayUrl():
+    queryUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=Stevenage,uk&mode=json&cnt=1&units=metric&APPID='
+    return queryUrl    
 
-def queryWeatherAPI():
-
+def queryWeatherAPI(queryUrl):
     # Load the API key from file
     f = open('myApiKey', 'r');
     apiText = f.read();
     f.close();
     # remove line break
     apiText.rstrip();
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=Stevenage,uk&APPID=' + apiText
-    response = requests.get(url);
+    queryUrl = queryUrl + apiText
+    response = requests.get(queryUrl);
     json_out = response.json;
     return json_out
 
-def assembleWeatherString(json_out):
+def assembleCurrentWeatherString(json_out):
 	location = json_out['name'];
 	weather  = json_out['weather'][0]['description'];
-	weatherString = "The weather in " + location + " right now is " + weather + "." 
+	weatherString = "The weather in " + location + " right now is " + weather + ". " 
 	return weatherString
 
+def assembleForecastForTheDayString(json_out):
+    temperature_min = str(json_out['list'][0]['temp']['min']);
+    temperature_max = str(json_out['list'][0]['temp']['max']);
+    rain_chance     = str(json_out['list'][0]['rain']);
+    rain_chance     = str(round(float(rain_chance) * 100));
+    wind_speed      = str(json_out['list'][0]['speed']);
+    weatherString = "The minimum temperature will be " +  temperature_min + " degrees, " \
+    "the maximum will be " + temperature_min + ". The chance of rain is " + rain_chance + "percent," \
+    " the wind speed is " + wind_speed + " meters per second."
+    return weatherString
 
 #some useful debug commands
 #download a speech file from google
 #downloadSpeechFromText("hello, how are you today", "./downloadedFile.mp3")
 #output phrase to audio
-json_out = queryWeatherAPI();
-weatherString = assembleWeatherString(json_out);
-speakSpeechFromText(weatherString)
+json_out = queryWeatherAPI(currentWeatherUrl());
+weatherString1 = assembleCurrentWeatherString(json_out);
+
+
+json_out = queryWeatherAPI(forecastDayUrl());
+weatherString2 = assembleForecastForTheDayString(json_out);
+
+fullMessage = weatherString1 + weatherString2 + " Have a nice day!"
+splitString = fullMessage.split('. ');
+
+for str in splitString:
+    speakSpeechFromText(str);
+
+
+
+
+
+
 
