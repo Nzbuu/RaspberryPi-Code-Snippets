4ddbd9bf8304486bae0cac5d9f38fc2fef4d632f
unknown <DRIA@ST-L-0038.tessella.co.uk>
2014-12-03 01:19:56 +0000

    Convert thermistor resistance to temperature using Steinhart-Hart equation.

--- ChargingCapacitor/script.py
+++ ChargingCapacitor/script.py
@@ -36,11 +36,25 @@ def main():
     timeCharge = countToHigh(pin_in, pin_out);
 
     timeConstant = timeCharge/math.log(2);
-    print "Time constant: " + str(timeConstant) + "s."
+    print "Time constant: " + str(round(timeConstant*1000,1)) + "ms."
     C = 10e-6; #[F]
     R = timeConstant / C;
 
-    print "R = " + str(R/1000) + "kOhm"
+    # Use Steinhart and Hart Equation to calculate temperature
+    A = 3.354016E-03;
+    B = 2.569850E-04;
+    C = 2.620131E-06;
+    D = 6.383091E-08;
+    R_ref = 10000;
+
+    lr = math.log(R/R_ref);
+    inverseOfT = A + B * lr + C * math.pow(lr,2) + D * math.pow(lr,3);
+
+    T = 1/inverseOfT;
+    T_deg = T -273.15;
+
+    print "R = " + str(round(R/1000,1)) + "kOhm"
+    print "Temperature = " + str(round(T_deg,1)) + " degC"
 
     print "wait for capacitor discharge"
     time.sleep(5 * timeConstant);
