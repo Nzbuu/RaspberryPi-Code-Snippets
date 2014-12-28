94b2a835d47b0e41bae148e2a69ca466e4ed7345
Anniek <anniekvanderdrift@hotmail.com>
2014-12-28 20:50:42 +0000

    Migrate changes of measurement class into database class

--- SensorAcquisition/DataBase.py
+++ SensorAcquisition/DataBase.py
@@ -1,7 +1,7 @@
 __author__ = 'DRIA'
 import sqlite3
 import json
-from measurement import measurement
+from Measurement import Measurement
 
 
 class DataBase:
@@ -16,11 +16,11 @@ class DataBase:
 
     def create_database_table(self, table_name, **kwargs):
         # Expected input format:
-        #create_database_table(data time = 'REAL', value = 'REAL')
-
+        # create_database_table(data, time = 'REAL', value = 'REAL')
+        #TODO: check if table already exists. Don't allow creating an existing table
         cursor = self.db.cursor()
 
-        sql_str = 'CREATE TABLE ' + table_name + self._assemble_create_args(kwargs)
+        sql_str = 'CREATE TABLE ' + table_name + _assemble_create_args(**kwargs)
 
         cursor.execute(sql_str)
         cursor.close()
@@ -28,16 +28,13 @@ class DataBase:
 
     def write_measurement_to_database(self, measurement, table_name):
         # This function writes to the db
-        # the format of measurement is expected be a dictionary, e.g.
-        # measurement = {'temperature':20, 'time': 10201}
-
         # TODO: check if the table exists before trying to insert data into it
-        meas = measurement.convert_to_dict(measurement)
+        meas = Measurement.convert_to_dict(measurement)
 
         list_of_keys = meas.keys()
         list_of_values = [meas[key] for key in list_of_keys]
 
-        sql_str = 'INSERT INTO  ' + table_name + self._assemble_insert_args(meas)
+        sql_str = 'INSERT INTO  ' + table_name + ' ' + _assemble_insert_args(meas)
 
         cursor = self.db.cursor()
         cursor.execute(sql_str, list_of_values)
@@ -58,21 +55,21 @@ class DataBase:
         cursor.close()
         return result
 
+
 def _assemble_insert_args(measurement):
-    # example measurement = {'temperature':20, time: 10201, 'humidity': 67%}
-    # expected output for measurement '(time, temperature, humidity) VALUES(?,?,?)'
+    # expected output for measurement '(timeStamp, values, units) VALUES(?,?,?)'
     list_of_keys = measurement.keys()
     keys = ','.join(list_of_keys)
 
     # make list of question marks the size of the length of the list of keys
     question_mark_list_length = len(list_of_keys)
-    qm_list = ','.join(['?' for x in range(0,question_mark_list_length)])
+    qm_list = ','.join(['?' for x in range(0, question_mark_list_length)])
 
     string_out = '(' + keys + ')' + ' VALUES (' + qm_list + ')'
     return string_out
 
-def _assemble_create_args(**kwargs):
 
+def _assemble_create_args(**kwargs):
     table_contents = '{keyName} {valueName}'
     table_contents = [table_contents.format(keyName=key, valueName=kwargs[key]) for key in kwargs]
 
--- SensorAcquisition/test_dataBase.py
+++ SensorAcquisition/test_dataBase.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
-from DataBase import DataBas
-from measurement import measurement
+from DataBase import DataBase
+from Measurement import Measurement
 
 __author__ = 'Anniek'
 
@@ -8,13 +8,12 @@ __author__ = 'Anniek'
 class TestDataBase(TestCase):
     def test_create_database_table(self):
         o = DataBase('testDB')
-        o.create_database_table('data', time='REAL', temperature='REAL', humidity='REAL')
+        o.create_database_table('data', timeStamp='REAL', value='REAL', units='TEXT')
         # TODO: insert assertion
 
     def test_write_measurement_to_database(self):
         o = DataBase('testDB')
-        #todo: use measurement class
-        data_point = {'temperature': 20, 'time': 10201, 'humidity': 67}
+        data_point = Measurement(value=20, timeStamp=10201, units='degC')
         o.write_measurement_to_database(data_point, 'data')
         # TODO: insert assertion
 
@@ -23,9 +22,8 @@ class TestDataBase(TestCase):
         results = o.read_from_database('data')
         # TODO: insert assertion
 
-    def write_array_of_measurements_to_database(self):
-        #todo: use measurement class
-        data_point = {'temperature': 20, 'time': 10201, 'humidity': 67}
+    def test_write_array_of_measurements_to_database(self):
+        data_point = Measurement(value=20, timeStamp=10201, units='degC')
         array_of_points = [data_point, data_point, data_point]
         o = DataBase('testDB')
 
