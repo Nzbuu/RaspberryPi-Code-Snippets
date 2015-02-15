8b8a2e9fd8599791b2a5249178a4b3356c3f2b51
Anniek van der Drift <anniekvanderdrift@hotmail.com>
2015-02-15 15:00:44 +0000

    Fix path definitions in data reading test (this test is a bit rubbish anyway)

--- Sensors/test_readData.py
+++ Sensors/test_readData.py
@@ -1,10 +1,11 @@
 __author__ = 'DRIA'
 import sys
 import os
-sys.path.append(os.path.realpath('../SensorAcquisition'))
+import json
+sys.path.append(os.path.realpath('../Sensors'))
 from DataBase import DataBase
 
-db = DataBase('../SensorAcquisition/DB/SensorData')
+db = DataBase('../Sensors/DB/SensorData')
 results = db.read_from_database('DS18B20', 5000) # limit to 96 data points for now
 
 time = [row.timeStamp for row in results]
